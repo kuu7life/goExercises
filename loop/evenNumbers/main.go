c5a9b7bf608d02ac50fd00ec463b3cd7eaa738be
kuu7life <kuu7life@gmail.com>
2022-02-14 00:17:43 +0800

    even numbers

---
+++ loop/evenNumbers/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	args := os.Args
+	if len(args) != 3 {
+		fmt.Println("Give me two numbers")
+		return
+	}
+
+	min, err1 := strconv.Atoi(args[1])
+	max, err2 := strconv.Atoi(args[2])
+
+	if err1 != nil || err2 != nil || min >= max {
+		fmt.Println("Wrong numbers")
+		return
+	}
+
+	var sum int
+
+	for i := min; i <= max; i++ {
+		if i%2 == 0 {
+			sum += i
+			fmt.Print(i)
+			if i < max {
+				fmt.Print(" + ")
+			}
+		}
+	}
+	fmt.Printf(" = %d\n", sum)
+}
